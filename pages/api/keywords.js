5fcab4b4b0e38fd12eacc0f9dff90147a4288c18
Nick <nickwong680@gmail.com>
2020-08-16 16:54:28 +1000

    done - sentiment analysis using AWS comprehend

--- pages/api/keywords.js
+++ pages/api/keywords.js
@@ -1,25 +1,51 @@
 //TypeScript
 import { ComprehendClient } from '@aws-sdk/client-comprehend-node/ComprehendClient';
-import { BatchDetectDominantLanguageCommand } from '@aws-sdk/client-comprehend-node/commands/BatchDetectDominantLanguageCommand';
+import { BatchDetectKeyPhrasesCommand } from '@aws-sdk/client-comprehend-node/commands/BatchDetectKeyPhrasesCommand';
+import sampleDataSet from '../../data/speeches';
+var v = require('voca');
 
 export default (req, res) => {
+  let sampleData = sampleDataSet.map((d) => d.data);
   const comprehend = new ComprehendClient({ region: 'ap-southeast-2' });
-  //clients can be shared by different commands
   const params = {
     LanguageCode: 'en',
-    TextList: [
-      'Let us keep before us the noble idea of a white Australia—snow-white Australia if you will. Let it be pure and spotless.',
-    ],
+    TextList: sampleData,
   };
-  const batchDetectDominantLanguageCommand = new BatchDetectDominantLanguageCommand(
-    params
-  );
+  const batchDetectKeyPhrasesCommand = new BatchDetectKeyPhrasesCommand(params);
   comprehend
-    .send(batchDetectDominantLanguageCommand)
+    .send(batchDetectKeyPhrasesCommand)
     .then((data) => {
-      console.log('data');
-      console.log(data);
-      console.log(data['ResultList'][0]);
+      data['ResultList'].map((i) => {
+        i['KeyPhrases'].map((keyPhrases, i) => {
+          sampleData[i] = v.replace(
+            sampleData[i],
+            keyPhrases['Text'],
+            '<b>' + keyPhrases['Text'] + '</b>'
+          );
+        });
+      });
+      return sampleData;
+      // {
+      //   Index: 0,
+      //   KeyPhrases: [
+      //     {
+      //       Score: 0.9999995827674866,
+      //       Text: 'the noble idea',
+      //       BeginOffset: 22,
+      //       EndOffset: 36
+      //     },
+      //     {
+      //       Score: 0.9999999403953552,
+      //       Text: 'a white Australia—snow-white Australia',
+      //       BeginOffset: 40,
+      //       EndOffset: 78
+      //     }
+      //   ]
+      // }
+    })
+    .then((data) => {
+      res.statusCode = 200;
+      res.json(data);
     })
     .catch((error) => {
       console.log(error);
